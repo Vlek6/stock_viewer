13b7bf6297bbd6192da2e4995afdd53f3d5e7292
Vlek6 <104684306+Vlek6@users.noreply.github.com>
2024-04-27 18:17:35 +0200

    Create db migration for StockName -> StockSymbol change

---
+++ Backend/StockViewer.Api/Data/Migrations/20240427161349_ChangeToStockSymbol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockViewer.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeToStockSymbol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StockName",
+                table: "Stocks",
+                newName: "StockSymbol");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StockSymbol",
+                table: "Stocks",
+                newName: "StockName");
+        }
+    }
+}
